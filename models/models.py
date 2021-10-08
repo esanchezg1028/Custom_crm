f353aca414e1e719fd95d6c797c1b775399c54ff
Elvia Lucia Sanchez garcia <esanchez@pragmaticingenieria.com>
2021-10-08 11:46:44 -0500

    ORM (Object-Relational mapping) Botones: Crear Actulizar/Editar Eliminar

--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from odoo import models, fields, api
 
 
@@ -16,3 +18,44 @@ class visit(models.Model):
 
     def toggle_state(self):
         self.done = not self.done
+
+    def f_create(self):
+        visit = {
+            'name': 'ORM test',
+            'customer': 1,
+            'date': str(datetime.datetime(2021, 8, 6)),
+            'type': 'P',
+            'done': False
+        }
+        print(visit)
+        self.env['custom_crm.visit'].create(visit)
+
+    def f_search_update(self):
+        visit = self.env['custom_crm.visit'].search([('name', '=', 'ORM test')])
+        print('search()', visit, visit.name)
+
+        visit_b = self.env['custom_crm.visit'].browse([4])
+        print('browse()', visit_b, visit_b.name)
+
+        visit.write({
+            'name': 'ORM test write'
+        })
+
+    def f_delete(self):
+        visit = self.env['custom_crm.visit'].browse([3])
+        visit.unlink()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
